7354aa10f6ea846cc7df8db7396e993dac30a5de
SupriyaMalla <supriyamalla@softwel.com.np>
2023-06-01 10:34:23 +0545

    Initial Commit

---
+++ TwitterSwiftUIPrac/TwitterSwiftUIPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIPracApp.swift
+//  TwitterSwiftUIPrac
+//
+//  Created by Aviyaan on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
